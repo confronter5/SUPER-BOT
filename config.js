9236267b40fb47e745ca3996541cd730271e6226
confronter5 <kiongosimkuu@gmail.com>
2025-04-30 09:58:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,8 @@ import moment from 'moment-timezone';
 global.botnumber = "254111728536"
 global.confirmcode = ""
 global.authFile = `BumbleSession`;
+global.isBaileysFail = false
+
 
 global.owner = [
   ['254794597254', '👑 𝐒𝐔𝐏𝐄𝐑 𝐁𝐎𝐓 𝐂𝐑𝐄𝐀𝐓𝐎𝐑👑', true],
